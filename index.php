c0a4f237d11b9c1174e61f80fe57a7406ee3281d
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2024-12-27 08:47:57 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+  <head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=devise-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet"type="text/css"href="BEBOL/Accueil.CSS"/>
+    <link rel="shortcut icon" type="image/x-icon" href="BEBOL/PHOTOS/IBMprodV.png">
+    <title>IBMprod</title>
+    
+  </head>
+  <body>
+    <h1>
+      <header class="navbar">
+        
+                    <!--Barre de navigation-->
+            
+        <div class="menu-icon" onclick="toggleMenu()">&#9776;</div>
+        <div class="nav-menu">
+          <a class="logo" href="#"><img src="BEBOL/PHOTOS/IBMprodS.png" alt="logo de l'entreprise"></a>
+        </div>
+          
+        <div class="menu" id="menuLinks">
+          
+          <a href="BEBOL/Cours.php">Cours</a></li> 
+          <a href="BEBOL/Publicité.php">Marketing</a></li>
+          <a href="BEBOL/Galerie.php">Galerie</a></li> 
+          <a href="BEBOL/Contacts.php">Contacts</a></li>
+          <a href="BEBOL/À propos .docx">À propos</a></li>
+                
+        </div>
+        <script>
+      
+          function toggleMenu() {
+          const menu = document.getElementById('menu');
+          menu.classList.toggle('active');
+          }
+        </script>      
+      </header>
+    </h1>
+
+
+                                    <!--Contenue-->
+    <h2>
+      <div class="bienvenu">BIENVENU(E)!</div>
+        <hr> 
+      <div class="service">
+        <p>Merci d'avoir choisi "BEBOL" comme votre assistant web,de faire confiance à l'entréprise IBMprod et de bien vouloir traiter un partenariat commercial.
+          <br>Ce pendant nous vous demandons de lire minutieusement notre <a href="BEBOL/Politique commerciale.pdf">Consigne d'utilisasion</a> avant de conclure un accord pour un projet,car l'entreprise qui est à caractère commercial ne traite  et conclue des accords et/ou contrats qu'avec des adultes (+18 ans)
+        </p>
+        <p>Toute fois,IBMprod ne ferme jamais ces portes aux mineurs (-18 ans).si l'utilisateur est mineur,il doit être accompagné de son tuteur pour un contrat. 
+          En ce qui concerne notre emploie du temps,nos portes sont ouverte de 09H à 18H:<br>lundi<br>Mardi<br>Mercredi<br>Jeudi<br>Vendredi<br>Sameedi(09H à 15H)
+        </p>
+        <p>Pour mieux  vous servir de près ou de loin,IBMprod met à votre disposition l'accès facil à nos services.</p>
+      </div>
+    </h2>    
+                          <!-- Lien vers le fichier JavaScript -->  
+     
+  </body>
+
+                                    <!--FOOTER-->
+
+  <footer>
+    <p1>IBMprod45@gmail.com   <pA>IBMprod  <pB> +243853193403</p>
+    <p2>copyright &copy;BEBOL2022 par IBMprod</p>
+  </footer>
+  
+</html>
