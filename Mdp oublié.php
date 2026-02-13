e8ec452ecff2e028ce44c70204fcff5037c3e703
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2026-02-13 03:21:20 +0100

    Update Mdp oublié.php

--- Mdp oublié.php
+++ Mdp oublié.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr-FR">
     <head>
-        <meta charset="UTF-8"/>
-        <meta name="viewport" content="width=devise-width, initial-scale=1.0">
-        <title>IBMprod</title>
-        <link rel="stylesheet"type="text/css"href="Projet.css"/>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Mdp oublié - BEBOL</title>
+        <link rel="stylesheet" type="text/css" href="Insc.css" />
+        <link rel="shortcut icon" type="image/x-icon" href="PHOTOS/BEBOL2S.png">
     </head>
 
 
 
 
     <footer>
-        <p1>IBMprod45@gmail.com   <pA>IBM_prod  <pB> +243853193403</p>
+        <p1>ibmprod45@gmail.com   <pA>IBMprod  <pB> +243853193403</p>
         <p2>copyright &copy;BEBOL2022 par IBMprod</p>
    </footer>
+
 </html>
