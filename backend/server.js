15fdeb2d41138298ea442b26ebf342dd196d4dd0
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2026-05-14 16:06:08 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+require("dotenv").config();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = require("./app");
+const connectDB = require("./config/database");
 
-// Middleware setup
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
+connectDB();
 
-// Routes initialization
-app.get('/', (req, res) => {
-    res.send('Welcome to the Express Server!');
-});
-
-// Error handling
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something went wrong!');
-});
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
