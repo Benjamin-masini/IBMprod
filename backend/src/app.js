1155434dd53c6b9811b00db1970059174cbda3f5
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2026-05-16 03:06:52 +0100

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,67 +1,13 @@
 const express = require("express");
-const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 // ROUTES
-app.use(
-  "/api/auth",
-  require("./modules/auth/auth.routes")
-);
-
-app.use(
-  "/api/users",
-  require("./modules/users/users.routes")
-);
-
-app.use(
-  "/api/properties",
-  require("./modules/property/property.routes")
-);
-
-app.use(
-  "/uploads",
-  express.static("uploads")
-);
-
-app.use(
-  "/api/favorites",
-  require("./core/favorites/favorite.routes")
-);
-
-app.use(
-  "/api/messaging",
-  require(
-    "./core/messaging/messaging.routes"
-  )
-);
-
-app.use(
-  "/api/marketplace",
-  require(
-    "./modules/marketplace/marketplace.routes"
-  )
-);
-
-app.use(
-  "/api/library",
-  require(
-    "./modules/library/library.routes"
-  )
-);
-
 app.use(
   "/api/events",
   require("./modules/event/event.routes")
 );
 
-app.use(
-  "/api/notifications",
-  require(
-    "./core/notifications/notification.routes"
-  )
-);
 module.exports = app;
