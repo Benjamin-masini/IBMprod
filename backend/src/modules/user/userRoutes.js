d721f84c59a0c61875ee231ca66253260279adaf
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2026-05-27 03:20:51 +0100

    Update userRoutes.js

--- backend/src/modules/user/userRoutes.js
+++ backend/src/modules/user/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/userController');
-const authMiddleware = require('../middleware/authMiddleware');
+const UserController = require('../controllers/userController.js');
+const authMiddleware = require('../middleware/authMiddleware.js');
 
 router.get('/', authMiddleware, UserController.getAllUsers);
 router.get('/:id', authMiddleware, UserController.getUserById);
