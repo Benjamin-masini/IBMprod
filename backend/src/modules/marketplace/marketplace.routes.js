821e2b9b0e21d7c638990e941af86b4659732577
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2026-05-27 04:03:17 +0100

    Update marketplace.routes.js

--- backend/src/modules/marketplace/marketplace.routes.js
+++ backend/src/modules/marketplace/marketplace.routes.js
@@ -3,19 +3,19 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require(
-  "./marketplace.controller"
+  "./marketplace.controller.js"
 );
 
 const auth = require(
-  "../../middleware/auth.middleware"
+  "../../middleware/auth.middleware.js"
 );
 
 const role = require(
-  "../../middleware/role.middleware"
+  "../../middleware/role.middleware.js"
 );
 
 const upload = require(
-  "../../middleware/upload.middleware"
+  "../../middleware/upload.middleware.js"
 );
 
 // GET ALL ITEMS
