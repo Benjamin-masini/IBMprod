16690f54ea1758c246361fca067aa22bb5f3061e
Benjamin Masini <142990404+Benjamin-masini@users.noreply.github.com>
2026-05-28 16:32:46 +0100

    Update property.routes.js

--- backend/src/modules/property/property.routes.js
+++ backend/src/modules/property/property.routes.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require(
-  "./property.controller"
+  "./property.controller.js"
 );
 
 const auth = require(
